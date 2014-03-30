ac322cc53976ae511b4dad01f340e2a0b6cb68d4
nir0s <nir36g@gmail.com>
2014-03-30 16:57:58 +0300

    CFY-179 added agent package installation and config urls

--- cloudify_openstack/cloudify_openstack.py
+++ cloudify_openstack/cloudify_openstack.py
@@ -71,6 +71,7 @@ DEFAULTS_CONFIG_FILE_NAME = 'cloudify-config.defaults.yaml'
 CLOUDIFY_PACKAGES_PATH = '/cloudify'
 CLOUDIFY_COMPONENTS_PACKAGE_PATH = '/cloudify3-components'
 CLOUDIFY_PACKAGE_PATH = '/cloudify3'
+AGENT_PACKAGES_PATH = '/cloudify-agents'
 
 verbose_output = False
 
@@ -582,6 +583,16 @@ class CosmoOnOpenStackBootstrapper(object):
                               'configured location in the config file')
                     return False
 
+                lgr.info('downloading cloudify ubuntu agent...')
+                r = self._download_package(
+                    AGENT_PACKAGES_PATH,
+                    cosmo_config['cloudify_ubuntu_agent_url'])
+                if not r:
+                    lgr.error('failed to download cloudify ubuntu agent. '
+                              'please ensure package exists in its '
+                              'configured location in the config file')
+                    return False
+
                 lgr.info('unpacking cloudify packages...')
                 r = self._unpack(
                     CLOUDIFY_PACKAGES_PATH)
@@ -606,6 +617,13 @@ class CosmoOnOpenStackBootstrapper(object):
                                       celery_user, mgmt_ip))
                 if not r:
                     lgr.error('failed to install cloudify')
+                    return False
+
+                lgr.info('unpacking cloudify agent...')
+                r = self._unpack(
+                    AGENT_PACKAGES_PATH)
+                if not r:
+                    lgr.error('failed to unpack cloudify agent')
                     return False
 
                 if dev_mode:
