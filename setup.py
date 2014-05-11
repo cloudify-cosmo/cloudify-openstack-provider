e41dadf193c99ba94cdbb98c21bf82ed2e971a2f
nir0s <nir36g@gmail.com>
2014-05-11 12:36:10 +0300

    release

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ __author__ = 'ran'
 
 from setuptools import setup
 
-VERSION = '1.0b1'
+VERSION = '1.0'
 
-COSMO_CLI_VERSION = '3.0b1'
+COSMO_CLI_VERSION = '3.0'
 COSMO_CLI_BRANCH = 'master'
 COSMO_CLI = 'https://github.com/cloudify-cosmo/cloudify-cli/tarball/' \
             '{0}#egg=cloudify-cli-{1}'.format(COSMO_CLI_BRANCH,
