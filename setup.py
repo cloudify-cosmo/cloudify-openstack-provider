ddf7f9c6f3032570d41b284c162c8595080ab674
opencm <limor@gigaspaces.com>
2015-01-28 09:07:30 +0200

    Bump version to 1.2m4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-openstack-provider',
-    version='1.2a3',
+    version='1.2a4',
     author='ran',
     author_email='ran@gigaspaces.com',
     packages=['cloudify_openstack'],
@@ -30,6 +30,6 @@ setup(
         'python-keystoneclient==0.7.1',
         'python-neutronclient==2.3.9',
         'IPy==0.81',
-        'cloudify==3.2a3',
+        'cloudify==3.2a4',
     ]
 )
